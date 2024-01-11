b3bd114679e9724c96a50076cfbdfdc7c8958635
MuhammadNizamani <ishaque.niz786@gmail.com>
2024-01-11 15:13:45 +0500

    ENH: added varible in go

--- booking-app/go.mod
+++ booking-app/go.mod
@@ -1,3 +1,3 @@
 module booking-app
 
-go 1.13
+go 1.21.6
--- booking-app/main.go
+++ booking-app/main.go
@@ -1,6 +1,14 @@
 package main
 
 import "fmt"
-func  main()  {
-	fmt.Println("Hello World")
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 49
+	// fmt.Println(conferenceTickets)
+	fmt.Printf("Welcome to %v booking application \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("get your tickets here to attend")
+
 }
