ef7f39fd5c2321892e08e287a53e4bfcb2c6f7ab
MuhammadNizamani <ishaque.niz786@gmail.com>
2024-01-12 14:27:12 +0500

    ENH: slice in go

--- booking-app/main.go
+++ booking-app/main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("get your tickets here to attend")
 
-	var bookings [50]string
+	var bookings []string
 	var firstName string
 	var lastName string
 	var email string
@@ -32,12 +32,13 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
+	// bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The whole slice: %v\n", bookings)
 	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	fmt.Printf("Scile type: %T\n", bookings)
+	fmt.Printf("Sclie length: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
